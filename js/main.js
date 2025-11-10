42ff7036277561d48ddf315333ed5d91661a1dcd
Rizky subekty <rizky.subekty@iconpln.co.id>
2025-11-11 01:58:47 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  async function loadComponent(selector, filePath) {
+    const el = document.querySelector(selector);
+    if (!el) return;
+    try {
+      const res = await fetch(filePath);
+      if (!res.ok) throw new Error(`Gagal memuat ${filePath}`);
+      const html = await res.text();
+      el.innerHTML = html;
+    } catch (err) {
+      console.error("Error memuat komponen:", filePath, err);
+    }
+  }
+
+
+  // Tentukan basePath berdasarkan URL
+  const basePath = (() => {
+    const pathname = window.location.pathname;
+
+    if (pathname.includes("/blogs/detail/")) {
+      return "../../../";
+    }
+
+    if (pathname.includes("/kampus/")) {
+        return "../../";
+    }
+
+    if (pathname.includes("/program/")) {
+        return "../../";
+    }
+
+    if (pathname.includes("/tentang-kami") ||
+        pathname.includes("/bimbelcpns") ||
+        pathname.includes("/bimbelpppk") ||
+        pathname.includes("/bimbelbumn") ||
+        pathname.includes("/blogs") ||
+        pathname.includes("/kontak-kami")) {
+      return "..";
+    }
+
+    return ".";
+  })();
+
+  // Muat header dan footer
+  await loadComponent("header", `${basePath}/components/header.html`);
+  await loadComponent("footer", `${basePath}/components/footer.html`);
+
+ 
+});
